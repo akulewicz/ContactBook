6f63e2625ee253a69faa1e89e4e0e6791afe1ad3
kula <a.kulewicz@gmail.com>
2023-03-26 18:34:20 +0200

    Fix employee class name

---
+++ ContactBook/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ContactBook
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Departament { get; set; }
+
+		public string PhoneNumber { get; set; }
+
+		public string Email { get; set; }
+	}
+}
+
